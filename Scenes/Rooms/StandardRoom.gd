8220c4333ac0d2127638377496f6b1c7372c4d08
robertarnborg <robert.arnborg@gmail.com>
2019-08-17 18:13:33 +0200

    notfunctioningscenechanger

--- Scenes/Rooms/StandardRoom.gd
+++ Scenes/Rooms/StandardRoom.gd
@@ -14,3 +14,16 @@ func check_enemy_amount():
 	monster_count -= 1
 	if monster_count <= 0:
 		get_tree().call_group("exits","activate_exit")
+
+
+func set_player_entrance_position():
+	if Global.level_entrance != null:
+		match Global.level_entrance:
+			"East": Global.player.position = $PlayerEntrancePos/East.position
+			"South": Global.player.position = $PlayerEntrancePos/South.position
+			"West": Global.player.position = $PlayerEntrancePos/West.position
+			"North":  Global.player.position = $PlayerEntrancePos/North.position
+			"NorthEast": Global.player.position = $PlayerEntrancePos/NorthEast.position
+			"SouthWest": Global.player.position = $PlayerEntrancePos/SouthWest.position
+			"NorthWest": Global.player.position = $PlayerEntrancePos/NorthWest.position
+			"SouthEast":  Global.player.position = $PlayerEntrancePos/SouthEast.position
