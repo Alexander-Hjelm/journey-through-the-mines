5e394e2f38525f4755d4e9cea3259e971ef880b5
Alexander Hjelm <nick.nertova@gmail.com>
2019-08-17 16:51:03 +0200

    Ranged projectiles

---
+++ Scenes/Projectile/ArrowProjectile.gd
@@ -0,0 +1,9 @@
+extends Projectile
+
+export(float) var _speed
+
+func _process(delta):
+	var angle = transform.get_rotation()
+	var fwd = Vector2(cos(angle), sin(angle))
+	position += fwd*_speed/100.0
+	print(position)
