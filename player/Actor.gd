a86fb8c0221b873f8ecfc43f36373a81f60a95ee
Alexander Hjelm <nick.nertova@gmail.com>
2019-08-17 14:37:35 +0200

    Hold mouse button to attack

--- player/Actor.gd
+++ player/Actor.gd
@@ -4,15 +4,19 @@ class_name Actor # Needed for inheritance
 export(PackedScene) var projectile
 export(String) var tag
 
+export var attack_interval = 1.0
 export var speed = 1.0
 export var hp = 3.0
 export var dmg = 1.0
+
+var _last_attack_time : float
 
 var animated_sprite
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	animated_sprite = get_node("AnimatedSprite")
+	_last_attack_time = Utils.get_time_seconds()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func set_velocity(vx, vy):
@@ -31,12 +35,14 @@ func set_velocity(vx, vy):
 		animated_sprite.flip_h = false
 	
 func attack(target_pos):
-	var projectile_instance : Projectile = projectile.instance()
-	add_child(projectile_instance)
-	projectile_instance.position = Vector2.ZERO
-	projectile_instance.look_at(target_pos)
-	projectile_instance.set_tag(tag)
-	projectile_instance.set_damage(dmg)
+	if Utils.get_time_seconds() - _last_attack_time > attack_interval:
+		var projectile_instance : Projectile = projectile.instance()
+		add_child(projectile_instance)
+		projectile_instance.position = Vector2.ZERO
+		projectile_instance.look_at(target_pos)
+		projectile_instance.set_tag(tag)
+		projectile_instance.set_damage(dmg)
+		_last_attack_time = Utils.get_time_seconds()
 	
 func apply_damage(dmg):
 	hp -= dmg
--- player/Player.gd
+++ player/Player.gd
@@ -15,7 +15,7 @@ func _process(delta):
 		vx += 1.0
 	if Input.is_action_pressed("ui_left"):
 		vx -= 1.0
-	if Input.is_action_just_pressed("attack"):
+	if Input.is_action_pressed("attack"):
 		attack(get_global_mouse_position())
 	
 	set_velocity(vx, vy)
